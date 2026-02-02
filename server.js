21dffa5e453c616f7aacd1d05c79cf7391e04f6d
nalinisekarsekar-oss <nalinisekarsekar@gmail.com>
2026-02-02 21:22:09 +0530

    mistake updated

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path")
 
 const app = express()
 
-app.use(express.urlencoded([extended=true]))
+app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
 app.get("/" ,(req,res)=>[
@@ -15,6 +15,33 @@ app.post("/send-email" , async(req ,res)=>{
     // app password = mnqi svvq qomt zito
  const {to, subject, body}=req.body;
  console.log(to, subject, body)
+  try {
+       const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: "465",
+      secure: true, 
+      auth: {
+        user: "nalinisekarsekar@gmail.com",
+        pass: "mnqisvvqqomtzito"
+      },
+      tls: {
+        rejectUnauthorized: false
+      }
+        });
+
+
+       const mailoptions = {
+      from: "nalinisekarsekar@gmail.com",
+      to: to,
+       subject: subject,
+      text: body
+    };
+    await transporter.sendMail(mailoptions)
+
+    res.send("mail send successfully")
+     }catch(err){
+    console.log(err)
+     }
 })
 app.listen(2000,()=>[
     console.log("sever is running on port 2000")
